c37d5fe939a13e594c8f28f00c9d245f84a655dc
Aleksandar Sarcevic <aleksandarsarcevic@protonmail.com>
2026-03-19 18:34:38 +0100

    feat: configure live Paddle checkout identifiers

--- paddle-config.js
+++ paddle-config.js
@@ -1,7 +1,7 @@
 window.PLAYMYSUBS_PADDLE_CONFIG = {
   environment: 'live',
-  clientToken: '',
-  priceId: '',
+  clientToken: 'live_3642498553a6b7f62b5a99fbc89',
+  priceId: 'pri_01km3jh4apayf3rdk94x39qf1j',
   successUrl: 'https://playmysubs.com/?checkout=success',
   locale: 'en',
   theme: 'light'
